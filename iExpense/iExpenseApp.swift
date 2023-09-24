658c5f1605d9f2e28b4fa39368a465f492de5fbc
Asfandyar <a_asfandyar1@hotmail.com>
2023-09-25 00:33:06 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Mac User on 25/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
